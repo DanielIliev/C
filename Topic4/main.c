f0d80eb7d3f71c11197ddae35b02954fdc23d828
DanielIliev <daniel.iliev.didiencho@gmail.com>
2019-03-16 02:34:55 +0200

    Update main.c

--- Topic4/main.c
+++ Topic4/main.c
@@ -1,6 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+FILE *menu();
+void readDirectory();
+FILE *readFile();
+
 int main() {
-	return 0;
+	FILE *fp;
+
+	return;
 }
+
+#include "menu.h"
+
+/*
+	Глупостите които разбирам:
+	Избор на файл за обработка
+	Потребителя праска символи докато ръчно не сложи край на програмата (Ctrl Z)
+	
+*/
+
+/*FILE *menu() {
+	system("cls");
+	printf("Menu items\n");
+	readDirectory();
+	FILE *fp = readFile();
+	return fp;
+}
+void readDirectory() {
+	DIR *d;
+	struct dirent *dir;
+	if ((d=opendir("."))!=NULL)
+	{
+		while ((dir=readdir(d)) != NULL) {
+			if (strcmp(dir->d_name, "..") > 0)
+			{
+				printf("%s\n", dir->d_name);
+			}
+		}
+	}
+	closedir(d);
+}
+FILE *readFile() {
+	FILE *fp;
+	char user_file[255];
+	int opened = 0;
+	while(opened==0) {
+		printf("Input the file name (as seen on screen):");
+		scanf("%255s", user_file);
+		if ((fp=fopen(user_file, "r")) != NULL)
+		{
+			printf("File opened\n");
+			opened=1;
+		} else {
+			printf("Bad name, please try again\n");
+			continue;
+		}
+	}
+	return fp;
+}*/
