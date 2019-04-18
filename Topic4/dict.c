4752fb45c6292db2b6884869020bdce0452791b7
DanielIliev <daniel.iliev.didiencho@gmail.com>
2019-04-18 10:40:19 +0300

    Update

--- Topic4/dict.c
+++ Topic4/dict.c
@@ -4,8 +4,8 @@ typedef struct {
     int id;
     char content[25];
     int count;
-}word;
-word words[500];
+}dict;
+dict words[500];
 
 void generateMenu() {
 	printf("\nPROGRAM DICTIONARY\n");
@@ -93,17 +93,43 @@ int checkSeparator(char c) {
     return check;
 }
 
+int checkWord(char word[]) {
+    int mempos=-1;
+    for(unsigned int i=0; i<ARRLEN(words); i++) {
+        if(strcmp(words[i].content, word) == 0) {
+            mempos=words[i].id;
+        }
+    }
+    return mempos;
+}
+
 void createDictionary(FILE *fp) {
-    int c;
+    int c=0,word_iter=0,mempos=0;
+    char *word = NULL;
+    word = (char*) calloc(25, sizeof(char));
     do {
         c = fgetc(fp);
         if(checkSeparator(c) == 0) {
-            printf("%c", c);
+            word[word_iter] = c;
+            word_iter++;
+        } else {
+            printf("%s\n", word);
+            /*if(checkWord(word) != -1) {
+                words[mempos].id = mempos;
+                strcpy(words[mempos].content, word);
+                words[mempos].count=1;
+                mempos++;
+            }
+            word = (void*) realloc(word, ARRLEN(word)*sizeof(char));
+            word_iter=0;*/
         }
     } while(c!=EOF);
 }
 
 void printDictionary() {
+    for(unsigned int i = 0; i<10; i++) {
+        printf("ID: %d, Content: %s, Count: %d\n", words[i].id, words[i].content, words[i].count);
+    }
 }
 
 void wordSearch() {
